61f920e628ac73aef76abdeda9969cebd6fe9bcb
KAKHA13 <kakhagiorgashvili@gmail.com>
2024-09-06 16:21:43 +0400

    init

--- app/app.js
+++ app/app.js
@@ -2,6 +2,11 @@ import Vue from 'nativescript-vue'
 
 import Home from './components/Home'
 
+Vue.registerElement(
+  "Mapbox",
+  () => require("@nativescript-community/ui-mapbox").MapboxView
+);
+
 new Vue({
   render: (h) => h('frame', [h(Home)]),
 }).$start()
--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,41 +1,66 @@
 <template>
     <Page>
         <ActionBar>
-            <Label text="Home"/>
+            <Label text="Home" />
         </ActionBar>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text.decode="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
+        <GridLayout rows="*">
+            <Mapbox
+                accessToken="your_token"
+                mapStyle="streets" latitude="41.716667" longitude="44.783333" hideLogo="false" hideCompass="false"
+                zoomLevel="5" showUserLocation="false" disableZoom="false" disableRotation="false" disableScroll="false"
+                disableTilt="false" @mapReady="onMapReady($event)">
+            </Mapbox>
         </GridLayout>
     </Page>
 </template>
 
 <script>
-  export default {
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
-    }
-  };
+export default {
+
+    methods: {
+        onMapReady(event) {
+            const mapboxView = event.map;
+
+            var onTap = function (marker) {
+                console.log("Marker tapped with title: '" + marker.title + "'");
+            };
+            var onCalloutTap = function (marker) {
+                alert("Marker callout tapped with title: '" + marker.title + "'");
+            };
+
+            mapboxView.addMarkers([
+                {
+                    id: 1, // can be user in 'removeMarkers()'
+                    lat: 41.7713151495571,
+                    lng: 44.15345008190155,
+                    title: 'One-line title here', // no popup unless set
+                    subtitle: 'Infamous subtitle!',
+                    // icon: 'res://cool_marker', // preferred way, otherwise use:
+                    // iconPath: res,
+                    selected: false,
+                    onTap: onTap,
+                    onCalloutTap: onCalloutTap
+                },
+            ]);
+
+
+        }
+    },
+};
 </script>
 
 <style scoped lang="scss">
-    @import '@nativescript/theme/scss/variables/blue';
+@import '@nativescript/theme/scss/variables/blue';
 
-    // Custom styles
-    .fas {
-        @include colorize($color: accent);
-    }
+// Custom styles
+.fas {
+    @include colorize($color: accent);
+}
 
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
+.info {
+    font-size: 20;
+    horizontal-align: center;
+    vertical-align: center;
+}
 </style>
